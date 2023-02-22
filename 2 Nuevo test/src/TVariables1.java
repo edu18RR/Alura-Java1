7b0c735f62e0c2f81e0c65e97a8c0d834a63cd74
edu18RR <edu.rosas.16@gmail.com>
2023-02-21 22:24:44 -0600

    Java 1er paso

---
+++ 2 Nuevo test/src/TVariables1.java
@@ -0,0 +1,12 @@
+public class TVariables1 {
+
+    public static void main(String [] args){
+
+        System.out.println("Hola nuevo test");    
+
+        int edad = 37;
+
+        System.out.println("La edad es "+ edad + ", Felicitaciones!");
+    }
+
+}
