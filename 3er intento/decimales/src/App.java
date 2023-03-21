b8c5df18538e8abc70743f839899bbe66e52a286
edu18RR <edu.rosas.16@gmail.com>
2023-03-20 20:18:37 -0600

    Paso a paso de double

--- 3er intento/decimales/src/App.java
+++ 3er intento/decimales/src/App.java
@@ -3,13 +3,13 @@ public class App {
         double salario = 1250.56;
         System.out.println(salario);
 
-        double edad = 28;
-        double salarioMitad = salario /2;
+        int edad = 28;
+        double salarioMitad = salario / 2;
         System.out.println(salarioMitad);
 
         //Primero se declara la variable 
-        int division;
-        division = 1250/ 3;
+        double division;
+        division = 1250.0 / 3.0;
         System.out.println(division);
     }
 }
---
+++ TestPuntoFlotante/src/TestPuntoFlotante.java
@@ -0,0 +1,12 @@
+public class TestPuntoFlotante {
+
+    public static void main(String[] args){
+
+        double salario = 1250.70;
+        System.out.println ("mi salario es $" + salario);    
+
+        double division = 5.0 / 2;
+        System.out.println (division);    
+    }
+
+}
